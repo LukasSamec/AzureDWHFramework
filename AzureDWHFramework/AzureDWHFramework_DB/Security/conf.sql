606ce1f5b7979bc43fa82b65fd3ef16b66294ea6
Lukáš Samec <Lukas.Samec@solitea.cz>
2022-10-19 20:49:23 +0200

    RebuildAllStageTables procedure

--- AzureDWHFramework/AzureDWHFramework_DB/Security/conf.sql
+++ AzureDWHFramework/AzureDWHFramework_DB/Security/conf.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [conf]
     AUTHORIZATION [dbo];
 
+
+
---
+++ AzureDWHFramework/AzureDWHFramework_DB/conf/Stored Procedures/p_RebuildAllStageTables.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE conf.p_RebuildAllStageTables
+AS
+
+DECLARE @SchemaName AS NVARCHAR(255)
+DECLARE @TableName AS NVARCHAR(255)
+
+DECLARE stageTable CURSOR FOR
+SELECT DISTINCT SchemaName, TableName from conf.StageTableMetadata
+
+OPEN stageTable
+FETCH NEXT FROM stageTable INTO @SchemaName, @TableName
+WHILE @@FETCH_STATUS = 0  
+BEGIN  
+
+       EXEC conf.p_RebuildStageTable @SchemaName, @TableName
+       FETCH NEXT FROM stageTable INTO @SchemaName, @TableName
+END;
+CLOSE stageTable;
+DEALLOCATE stageTable;
