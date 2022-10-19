f309fbdb470ee14be987e056e655e266884fb493
Lukáš Samec <Lukas.Samec@solitea.cz>
2022-10-19 22:36:49 +0200

    Framework log objects added

---
+++ AzureDWHFramework/AzureDWHFramework_DB/log/Stored Procedures/InsertFrameworkLog.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE log.InsertFrameworkLog
+@Type NVARCHAR(10),
+@Message NVARCHAR(MAX)
+AS
+INSERT INTO log.FrameworkLog(CreatedDate, Type, Message) 
+VALUES(GETDATE(), @Type, @Message)
---
+++ AzureDWHFramework/AzureDWHFramework_DB/log/Tables/FrameworkLog.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [log].[FrameworkLog] (
+    [FrameworkLogID] BIGINT         IDENTITY (1, 1) NOT NULL,
+    [CreatedDate]    DATETIME2 (7)  DEFAULT (getdate()) NOT NULL,
+    [Type]           NVARCHAR (10)  NOT NULL,
+    [Message]        NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([FrameworkLogID] ASC),
+    CONSTRAINT [Check_FrameworkLog_Message] CHECK ([Message]='Error' OR [Message]='Info')
+);
+
