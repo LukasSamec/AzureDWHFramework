fa1b50ed4d487453729e7d1954ec14a5db5db534
Lukáš Samec <Lukas.Samec@solitea.cz>
2022-10-19 20:10:30 +0200

    RebuildStageTable procedure

---
+++ AzureDWHFramework/AzureDWHFramework_DB/conf/Stored Procedures/p_RebuildStageTable.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [conf].[p_RebuildStageTable] 
+@TableSchema NVARCHAR(255),
+@TableName NVARCHAR(255)
+AS
+DECLARE @sql AS NVARCHAR(MAX)
+
+SET @sql = 'IF OBJECT_ID(''' + @TableSchema + '.' + @TableName + ''', N''U'') IS NOT NULL ' + 'DROP TABLE ' + @TableSchema + '.' + @TableName
+
+--print (@sql)
+execute sp_executesql @sql
+
+SET @sql = 
+'CREATE TABLE ' + @TableSchema + '.' + @TableName + ' (' +
+ (
+   SELECT STRING_AGG
+   (
+		CONCAT
+		(
+			'[', ColumnName, '] ', DataType, ' ', CASE WHEN Nullable = 1 THEN 'NULL' ELSE 'NOT NULL' END
+		) , ', '
+   ) 
+	FROM conf.StageTableMetadata WHERE TableName = @TableName AND SchemaName = @TableSchema
+ ) +
+',[InsertedID] BIGINT NOT NULL' +
+',[UpdatedID] BIGINT NOT NULL' +
+')'
+
+
+--print (@sql)
+execute sp_executesql @sql
---
+++ AzureDWHFramework/AzureDWHFramework_DB/conf/Tables/StageTableMetadata.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [conf].[StageTableMetadata] (
+    [StageTableMetadataID] INT            IDENTITY (1, 1) NOT NULL,
+    [SchemaName]           NVARCHAR (255) DEFAULT ('stage') NOT NULL,
+    [TableName]            NVARCHAR (255) NOT NULL,
+    [ColumnName]           NVARCHAR (255) NOT NULL,
+    [DataType]             NVARCHAR (255) NOT NULL,
+    [Nullable]             BIT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([StageTableMetadataID] ASC)
+);
+
