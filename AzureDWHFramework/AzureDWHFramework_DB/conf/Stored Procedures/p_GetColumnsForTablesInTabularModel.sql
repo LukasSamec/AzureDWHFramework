fff627aa9c89e6eac9a096da57cb05f3d530e843
Lukáš Samec <Lukas.Samec@solitea.cz>
2022-10-23 13:46:58 +0200

    relationship procedure

--- AzureDWHFramework/AzureDWHFramework_DB/conf/Stored Procedures/p_GetColumnsForTablesInTabularModel.sql
+++ AzureDWHFramework/AzureDWHFramework_DB/conf/Stored Procedures/p_GetColumnsForTablesInTabularModel.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE conf.p_GetColumnsForTablesInTabularModel
+﻿CREATE PROCEDURE [conf].[p_GetColumnsForTablesInTabularModel]
 @TabularModel NVARCHAR(255),
 @TableName NVARCHAR(255)
 AS
@@ -9,6 +9,15 @@ DataType
 FROM conf.DimensionTable dimTable
 INNER JOIN conf.TabularModel tabModel ON tabModel.TabularModelID = dimTable.TabularModelID
 INNER JOIN conf.DimensionTableColumn dimTableCol ON dimTableCol.DimensionTableID = dimTable.DimensionTableID
+WHERE TabularModelName = @TabularModel AND TableName = @TableName
+
+UNION ALL
+
+SELECT
+CONCAT(dimTable.TableName,'ID') ColumnName,
+'INT' DataType
+FROM conf.DimensionTable dimTable
+INNER JOIN conf.TabularModel tabModel ON tabModel.TabularModelID = dimTable.TabularModelID
 WHERE TabularModelName = @TabularModel AND TableName = @TableName
 
 UNION ALL
---
+++ AzureDWHFramework/AzureDWHFramework_DB/conf/Stored Procedures/p_GetRelationshipsForTabularModel.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE conf.p_GetRelationshipsForTabularModel
+  @TabularModel NVARCHAR(255)
+  AS
+  SELECT 
+  factTable.TableName TableN,
+  dimTable.TableName TableOne,
+  factTableCol.ColumnName
+  FROM 
+  conf.FactTable factTable 
+  INNER JOIN conf.FactTableColumn  factTableCol ON factTable.FactTableID = factTableCol.FactTableID
+  INNER JOIN conf.DimensionTable dimTable ON dimTable.DimensionTableID = factTableCol.DimensionTableID
+  INNER JOIN conf.TabularModel tabularModelFact ON tabularModelFact.TabularModelID = factTable.TabularModelID
+  INNER JOIN conf.TabularModel tabularModelDim ON tabularModelDim.TabularModelID = dimTable.TabularModelID
+  WHERE tabularModelFact.TabularModelName = @TabularModel AND  tabularModelDim.TabularModelName = @TabularModel
