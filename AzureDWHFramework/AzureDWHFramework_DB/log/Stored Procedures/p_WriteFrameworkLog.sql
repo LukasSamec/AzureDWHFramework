2f507956e9ad81de6f489275567d36c428f95f6d
Lukáš Samec <Lukas.Samec@solitea.cz>
2022-10-23 21:58:02 +0200

    write frameworklog

---
+++ AzureDWHFramework/AzureDWHFramework_DB/log/Stored Procedures/p_WriteFrameworkLog.sql
@@ -0,0 +1,8 @@
+﻿
+CREATE PROCEDURE [log].[p_WriteFrameworkLog]
+@ProcedureName NVARCHAR(100),
+@Type NVARCHAR(10),
+@Message NVARCHAR(MAX)
+AS
+INSERT INTO log.FrameworkLog(CreatedDate, Type, Message, ProcedureName) 
+VALUES(GETUTCDATE(), @Type, @Message, @ProcedureName)
