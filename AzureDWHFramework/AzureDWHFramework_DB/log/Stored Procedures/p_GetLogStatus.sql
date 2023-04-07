f51744c399138ba0d8463de424e012390be9a6ce
Lukáš Samec <Lukas.Samec@solitea.cz>
2023-04-07 18:12:45 +0200

    log status

---
+++ AzureDWHFramework/AzureDWHFramework_DB/log/Stored Procedures/p_GetLogStatus.sql
@@ -0,0 +1,15 @@
+﻿
+
+CREATE PROCEDURE [log].[p_GetLogStatus]
+  @ETLLogID INT 
+AS
+
+-- Vrácení hodnoty 'Failed' pokud se během ETL procesu objevila chyba při načítání jakékoli tabulky. Jinak se vrátí hodnota 'Success'.
+IF EXISTS (SELECT ETLTableLoadLogID from log.ETLTableLoadLog WHERE ETLLogID = @ETLLogID AND Status = 3)
+BEGIN
+		SELECT 'Failed' logStatus
+END
+ELSE
+BEGIN
+		SELECT 'Success' logStatus
+END
